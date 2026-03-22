9c7b451678f77009a7998ccf7e9e99b66d3ca62b
Manoz7 <manojkhanal936@gmail.com>
2026-03-21 23:58:33 -0500

    MBF

--- Inheritance/src/superkeyword/Child.java
+++ Inheritance/src/superkeyword/Child.java
@@ -1,5 +1,9 @@
 package superkeyword;
 
-public class Child {
+public class Child extends Parent{
+	void f1() {
+		super.f1();
+		System.out.println("Inside child f1");
+	}
 
 }
--- Inheritance/src/superkeyword/Parent.java
+++ Inheritance/src/superkeyword/Parent.java
@@ -3,7 +3,7 @@ package superkeyword;
 public class Parent {
 
 	void f1() {
-		System.out.println("Inside f1");
+		System.out.println("Inside parents f1");
 	}
 
 }
